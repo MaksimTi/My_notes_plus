b49c49e984d922bfdd04f0fabe59d58016f1bb91
Maksim_T <m_m_titov@mail.ru>
2021-06-14 13:42:11 +0300

    init commit.

---
+++ app/src/main/java/com/example/mynotesplus/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.mynotesplus.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Здесь будут новые заметки");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
